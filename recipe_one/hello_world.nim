62476c84528ff512dc0cafb2d9445648e65846e1
eindiran <eindiran@promptu.com>
2018-02-14 17:14:16 -0800

    Deleting old locations

--- recipe_one/hello_world.nim
+++
@@ -1,4 +0,0 @@
-# Nim Cookbook
-# Recipe 1 -- Hello World
-
-echo "Hello, World!"
--- recipe_two/greeting.nim
+++
@@ -1,6 +0,0 @@
-# Nim Cookbook
-# Recipe 2 -- Greetings
-
-echo "What is your name?\t"
-var name: string = readLine(stdin)
-echo "Hi, ", name, ". It is nice to meet you!"
